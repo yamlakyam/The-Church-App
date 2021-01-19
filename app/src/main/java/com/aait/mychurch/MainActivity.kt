f7fcf04eaba2798e4a4ffb6a0daa1ff53b4aeb66
yam <yamlakyam@gmail.com>
2021-01-19 15:57:33 +0300

    sign up activity completed

--- app/src/main/java/com/aait/mychurch/MainActivity.kt
+++ app/src/main/java/com/aait/mychurch/MainActivity.kt
@@ -15,5 +15,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        forgotPwLink.setOnClickListener {
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

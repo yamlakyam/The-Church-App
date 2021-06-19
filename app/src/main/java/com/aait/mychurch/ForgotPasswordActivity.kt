6885aabed187bef90c39fbc7c6221ef2db4375df
unknown <yamlakyam@gmail.com>
2021-06-19 12:01:59 +0300

    resetting password works

--- app/src/main/java/com/aait/mychurch/ForgotPasswordActivity.kt
+++ app/src/main/java/com/aait/mychurch/ForgotPasswordActivity.kt
@@ -2,10 +2,42 @@ package com.aait.mychurch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    lateinit var emailFP: String
+    val firebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        sendLinkBtn.setOnClickListener {
+            if (emailForgot.text.toString().trim().isEmpty()) {
+                emailForgot.error = "Email is required"
+                emailForgot.requestFocus()
+                return@setOnClickListener
+            }
+            emailFP = emailForgot.text.toString().trim()
+
+            firebaseAuth.sendPasswordResetEmail(emailFP).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "Reset email sent to $emailFP successfully!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Error sending email to $emailFP !",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+        }
     }
 }
